8d35d4fa2390a52306e288ba3447a6b10604ec8e
Derek Smith <Derek.S.Smith-1@ou.edu>
2020-01-22 12:34:07 -0600

    Created the in class examples solution and created project for variables

---
+++ in class solutions examples/variables/Program.cs
@@ -0,0 +1,16 @@
+﻿//Derek Smith
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
