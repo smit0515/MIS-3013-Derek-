2f141cb46690a2afdf4be3e08f48f51976015fb5
Derek Smith <Derek.S.Smith-1@ou.edu>
2020-01-29 12:42:59 -0600

    Coin Toss 50/50

---
+++ Participations/CoinToss/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinToss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("choose heads or tails");
+            string input = Console.ReadLine();
+            var rand = new Random();
+            var value = rand.Next(0, 2);
+
+            string answer = "";
+
+            if (value == 1)
+            {
+                answer = "heads";
+            }
+            else if (value == 0)
+            {
+                answer = "tails";
+
+
+                if (input == answer)
+                {
+                    Console.WriteLine("Congratulations, you guessed correctly");
+
+                }
+                else if (input != answer)
+                {
+                    Console.WriteLine("You suck at guessing");
+                }
+
+
+                Console.ReadKey();
+
+
+
+
+            }
+    }
+}
