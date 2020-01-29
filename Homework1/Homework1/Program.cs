0dd7fc43539028fd6d38151bf09540d5bed8f3cb
Derek Smith <Derek.S.Smith-1@ou.edu>
2020-01-29 13:15:04 -0600

    Homework 1, product selection

---
+++ Homework1/Homework1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please write the item you are wanting to purchase?");
+            string item = Console.ReadLine();
+            
+            Console.WriteLine("How many items are you wanting?");
+            
+
+
+
+
+
+
+
+            Console.ReadKey();
+
+
+        }
+       
+    }
+}
