efb4eef89818fc6f07b92b5735521d03b3d2264b
Derek Smith <Derek.S.Smith-1@ou.edu>
2020-02-02 15:52:35 -0600

    homework 1 part 1

--- Homework1/Homework1/Program.cs
+++ Homework1/Homework1/Program.cs
@@ -10,19 +10,31 @@ namespace Homework1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please write the item you are wanting to purchase?");
-            string item = Console.ReadLine();
-            
-            Console.WriteLine("How many items are you wanting?");
-            
+            const double SalesTax = .085;//8.5%
+            string itemname;
+            int amount;
+            double price;
 
+            Console.WriteLine("What is the name of the product you are trying to purchase");
+            itemname = Console.ReadLine();
 
+            Console.WriteLine("How many " + itemname + " do you want to buy?");
+            amount = int.Parse(Console.ReadLine());
 
+            Console.WriteLine("What is the price for each" + itemname + "?");
+            price = double.Parse(Console.ReadLine());
 
+            double subtotal = price * amount;
+            double salestax = SalesTax * subtotal;
+            double salestotal = subtotal + salestax;
 
+            Console.WriteLine("Your subtotal for your bill is" + subtotal.ToString("C") + "\nYour sales tax for your bill is" + salestax.ToString("C") + "\nYour total for your bill is" + salestotal.ToString("C"));
 
+            Console.WriteLine("Press any key to conntinue....");
 
-            Console.ReadKey();
+            Console.ReadLine();
+
+          
 
 
         }
